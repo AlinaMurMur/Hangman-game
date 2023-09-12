4aa792cda97062239fb3c3fc78dcc4e4f77529fe
AlinaMurMur <alinamurtazina777@yandex.ru>
2023-09-12 10:58:55 +0300

    Add hangman game files

---
+++ src/js/consts.js
@@ -0,0 +1,3 @@
+export const WORDS = ['cat', 'cow', 'dog', 'duck', 'donkey', 'goose', 'hamster', 'mouse', 'pig', 'rabbit', 'sheep', 'bear', 'crocodile', 'elephant', 'giraffe', 'fox', 'leopard', 'monkey', 'panda', 'snake', 'zebra', 'chicken', 'hedgehog', 'mole', 'squirrel', 'wolf', 'frog', 'lion', 'tiger', 'camel']
+
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
